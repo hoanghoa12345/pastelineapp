67e602acaf593022f772dd1611fcc1b3a084c278
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2023-08-17 21:20:36 +0700

    Add not found middleware

--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -7,5 +7,8 @@ router.use('/api/v1', v1);
 router.get('/', (req: Request, res: Response) => {
   res.onSuccess(200, 'Server is running...', { version: '1.0.1' });
 });
+router.get('*', (req: Request, res: Response) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
 
 export default router;
