6cf34871ce5522bed7ca067eb64d2e622e319d0a
hoanghoa12345 <44923253+hoanghoa12345@users.noreply.github.com>
2023-08-27 12:06:17 +0700

    Add pagination for all notes

--- src/composables/useAllNotes.ts
+++ src/composables/useAllNotes.ts
@@ -1,7 +1,22 @@
+import * as pagination from "@zag-js/pagination";
+import { normalizeProps, useMachine } from "@zag-js/vue";
+import { sortBy } from "lodash-es";
+import { format } from "date-fns";
+
 export function useAllNotes() {
   const notesStore = useNotesStore();
   const searchQuery = ref<string>("");
   const isConfirm = ref<boolean>(false);
+  const [state, send] = useMachine(
+    pagination.machine({
+      id: "1",
+      count: 0,
+    })
+  );
+
+  const api = computed(() =>
+    pagination.connect(state.value, send, normalizeProps)
+  );
 
   const getAll = () => {
     notesStore.getAll();
@@ -27,6 +42,52 @@ export function useAllNotes() {
     isConfirm.value = true;
   };
 
+  const notes = computed(() =>
+    sortBy(notesStore.notes, (obj) => {
+      return format(new Date(obj.updatedAt), "yyyyMMdd");
+    }).reverse()
+  );
+
+  const isLoading = computed(() => notesStore.isLoading);
+
+  watchEffect(() => {
+    getAll();
+  });
+
+  const checkAll = computed({
+    get: () =>
+      notes.value
+        ? notesStore.selectedNote.length == notes.value.length
+        : false,
+    set: (val) => {
+      let checked = [];
+      if (val) {
+        notes.value.forEach((item) => {
+          checked.push(item.noteId);
+        });
+      }
+      notesStore.selectedNote = checked;
+    },
+  });
+
+  const filterResult = computed(() => {
+    if (notes.value instanceof Array) {
+      return notes.value.filter(
+        (note) =>
+          note.title.toLowerCase().indexOf(searchQuery.value.toLowerCase()) > -1
+      );
+    }
+    return [];
+  });
+
+  watch(filterResult, (value) => {
+    api.value.setCount(value.length);
+  });
+
+  const currentPageData = computed(() =>
+    filterResult.value.slice(api.value.pageRange.start, api.value.pageRange.end)
+  );
+
   return {
     notesStore,
     getAll,
@@ -36,5 +97,10 @@ export function useAllNotes() {
     searchQuery,
     isConfirm,
     openConfirmModal,
+    filterResult,
+    checkAll,
+    isLoading,
+    api,
+    currentPageData,
   };
 }
