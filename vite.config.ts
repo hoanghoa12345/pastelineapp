9b09d4348ac94866b7fd44ce9d31e294e11793f6
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2024-05-20 19:12:56 +0700

    feat(gh-page): Update vite base path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [
-    vue(),
-    Components({
-      dirs: ["./src/components"],
-    }),
-    AutoImport({
-      dts: true,
-      dirs: ["./src/composables", "./src/stores"],
-      imports: ["vue", "vue-router"],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@/": "/src/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_PUBLIC_PATH || "/",
+    plugins: [
+      vue(),
+      Components({
+        dirs: ["./src/components"],
+      }),
+      AutoImport({
+        dts: true,
+        dirs: ["./src/composables", "./src/stores"],
+        imports: ["vue", "vue-router"],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@/": "/src/",
+      },
     },
-  },
-  server: {
-    port: 5173,
-  },
+    server: {
+      port: 5173,
+    },
+  };
 });
